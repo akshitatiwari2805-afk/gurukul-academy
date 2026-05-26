9f0e1ac63f506b0b3e1a87e155f632725f6da9f7
Akshita Tiwari <akshitatiwari2805@gmail.com>
2026-05-26 12:26:14 +0530

    final database url setup fixed

--- server.js
+++ server.js
@@ -70,7 +70,14 @@ app.get('/api/fetch', async (req, res) => {
 
 // MongoDB connection string
 mongoose.connect('mongodb+srv://akshitatiwari2805:akshi123@cluster0.p4yx.mongodb.net/gurukul?retryWrites=true&w=majority')
-  .then(() => console.log('MongoDB Connected & Cloudinary Linked Permanent!'))
-  .catch(err => console.error(err));
+  .then(() => {
+    console.log('MongoDB Connected & Cloudinary Linked Permanent!');
+  })
+  .catch((err) => {
+    console.error('MongoDB Connection Error:', err);
+  });
+
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server live on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server live on port ${PORT}`);
+});
